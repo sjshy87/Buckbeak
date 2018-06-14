ffc7b7c9d52793750036d7985b1aa7b6cd32907f
Ryan Festag <rfestag@gmail.com>
2018-06-13 21:29:15 -0600

    Adding PolarMap.js. Component needs to be implemented. Also starting to show how component documentation will be done

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,36 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid/dist/styles/ag-grid.css";
 import "ag-grid/dist/styles/ag-theme-balham-dark.css";
+import "ag-grid/dist/styles/ag-theme-balham.css";
 
-export default class RichGridDeclarativeExample extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      columnDefs: [
-        { headerName: "Make", field: "make" },
-        { headerName: "Model", field: "model" },
-        { headerName: "Price", field: "price" }
-      ],
-      rowData: [
-        { make: "Toyota", model: "Celica", price: 35000 },
-        { make: "Ford", model: "Mondeo", price: 32000 },
-        { make: "Porsche", model: "Boxter", price: 72000 }
-      ]
-    };
-  }
+export default class Grid extends Component {
+  static propTypes = {
+    /** Data for this grid. Each entry is a row in the grid */
+    data: PropTypes.array,
+    /** Column definitions for this grid */
+    columns: PropTypes.array,
+    /** The theme to use */
+    theme: PropTypes.string
+  };
+  static defaultProps = {
+    data: [
+      { make: "Toyota", model: "Celica", price: 35000 },
+      { make: "Ford", model: "Mondeo", price: 32000 },
+      { make: "Porsche", model: "Boxter", price: 72000 }
+    ],
+    columns: [
+      { headerName: "Make", field: "make" },
+      { headerName: "Model", field: "model" },
+      { headerName: "Price", field: "price" }
+    ],
+    theme: "ag-theme-balham-dark"
+  };
 
   render() {
+    console.log(this.props.data);
     return (
       <div
         style={{ height: "100%", width: "100%" }}
-        className="ag-theme-balham-dark"
+        className={this.props.theme}
       >
         <AgGridReact
           // binding to array properties
-          rowData={this.state.rowData}
-          columnDefs={this.state.columnDefs}
+          rowData={this.props.data}
+          columnDefs={this.props.columns}
         />
       </div>
     );
--- src/js/actions/map.js
+++ src/js/actions/map.js
@@ -1,11 +1,19 @@
 /* Types */
+export const SET_BASELAYER = "SET_BASELAYER";
 export const ADD_BASELAYER = "ADD_BASELAYER";
-export const ADD_OVERLAY = "ADD_OVERLAY";
 export const REMOVE_BASELAYER = "REMOVE_BASELAYER";
+
+export const SHOW_OVERLAY = "SHOW_OVERLAY";
+export const HIDE_OVERLAY = "HIDE_OVERLAY";
+export const ADD_OVERLAY = "ADD_OVERLAY";
 export const REMOVE_OVERLAY = "REMOVE_OVERLAY";
 
 /* Actions */
+export const setBaselayer = layer => ({ type: SET_BASELAYER, layer });
 export const addBaselayer = layer => ({ type: ADD_BASELAYER, layer });
-export const addOverlayer = layer => ({ type: ADD_OVERLAY, layer });
 export const removeBaselayer = layer => ({ type: REMOVE_BASELAYER, layer });
+
+export const showOverlay = layer => ({ type: SHOW_OVERLAY, layer });
+export const hideOverlay = layer => ({ type: HIDE_OVERLAY, layer });
+export const addOverlayer = layer => ({ type: ADD_OVERLAY, layer });
 export const removeOverlay = layer => ({ type: REMOVE_OVERLAY, layer });
